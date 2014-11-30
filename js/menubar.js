701bdca233a8dc6cc79bdb4f248df460233b7861
Rob van Loon <borrob@me.com>
2014-11-30 21:47:18 +0100

    small updates

--- js/menubar.js
+++ js/menubar.js
@@ -23,7 +23,6 @@ $('#menubar').html('<div class="container">'+
 					'<li><a href="#">2014</a></li>'+
 				'</ul>'+
 			'</li>'+
-			'<li><a href="#photos">Photos</a></li>'+
 			'<li id="menu_listen"><a href="listen.html">Listen</a></li>'+
 			'<li class="dropdown">'+
 				'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Ensembles <span class="caret"></span></a>'+
