bd69fa861177b97179c50d482560dd321e8d578c
Rob van Loon <borrob@me.com>
2014-12-02 23:58:51 +0100

    Added 2013 and 2014 news

--- js/menubar.js
+++ js/menubar.js
@@ -37,3 +37,43 @@ $('#menubar').html('<div class="container">'+
 		'</ul>'+
 	'</div><!--/.nav-collapse -->'+
 '</div>');
+
+$('#menubar1deep').html('<div class="container">'+
+	'<div class="navbar-header">'+
+		'<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">'+
+			'<span class="sr-only">Toggle navigation</span>'+
+			'<span class="icon-bar"></span>'+
+			'<span class="icon-bar"></span>'+
+			'<span class="icon-bar"></span>'+
+		'</button>'+
+		'<a class="navbar-brand" href="http://www.anneveinberg.com">Anne Veinberg</a>'+
+	'</div>'+
+	'<div id="navbar" class="navbar-collapse collapse">'+
+		'<ul class="nav navbar-nav">'+
+			'<li id="menu_home"><a href="../index.html">Home</a></li>'+
+			'<li id="menu_about"><a href="../about.html">About</a></li>'+
+			'<li id="menu_news"><a href="../news.html">News</a></li>'+
+			'<li id="menu_calendar" class="dropdown">'+
+				'<a href="../#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Calendar <span class="caret"></span></a>'+
+				'<ul class="dropdown-menu" role="menu">'+
+					'<li><a href="../calendar_upcoming.html">Upcoming</a></li>'+
+					'<li class="divider"></li>'+
+					'<li><a href="../#">2012</a></li>'+
+					'<li><a href="../#">2013</a></li>'+
+					'<li><a href="../#">2014</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li id="menu_ensembles" class="dropdown">'+
+				'<a href="../#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Ensembles <span class="caret"></span></a>'+
+				'<ul class="dropdown-menu" role="menu">'+
+					'<li id="menu_offzz"><a href="../offzz.html">Off&lt;&gt;zz</a></li>'+
+					'<li id ="menu_scala"><a href="../scala.html">SCALA</a></li>'+
+					'<li id="menu_kolthofveinberg"><a href="../duo_kolthof_veinberg.html">Duo Kolthof/Veinberg</a></li>'+
+					'<li id="menu_neshomme"><a href="../duo_neshomme.html">Duo Neshomme</a></li>'+
+					'<li id="menu_ha"><a href="../duo_ha.html">Duo H|A</a></li>'+
+				'</ul>'+
+			'</li>'+
+			'<li id="menu_contact"><a href="../contact.html">Contact</a></li>'+
+		'</ul>'+
+	'</div><!--/.nav-collapse -->'+
+'</div>');
